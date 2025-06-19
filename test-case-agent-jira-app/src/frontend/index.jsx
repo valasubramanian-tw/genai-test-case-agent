7de50e71c7695a8116360f0646786d6386cb8f39
Valasubramanian S <valasubramanian.s@thoughtworks.com>
2025-06-19 22:32:30 +0530

    delete old jira app

--- test-case-agent-jira-app/src/frontend/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ForgeReconciler from '@forge/react';
-import App from './app';
-
-ForgeReconciler.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
--- test-case-agent-jira-app/src/models/JiraStory.ts
+++
@@ -1,8 +0,0 @@
-interface JiraStory {
-    key: string;
-    summary: string;
-    description?: string;
-    status: string;
-}
-
-export default JiraStory;
